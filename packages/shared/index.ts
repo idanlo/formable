7805e5ef226c164898c384bb390bbaa5a8700862
Idan Lottan <idan2110@gmail.com>
2019-11-07 15:58:16 +0200

    Add types to shared package

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,3 +1,23 @@
-export function hello(): string {
-  return 'hello';
+export type FieldTypeRadio = {
+  type: 'radio';
+  radio: {
+    title: string;
+    options: Array<{ label: string; value: string }>;
+  };
+};
+
+export type FieldTypeQuestion = {
+  type: 'question';
+  question: {
+    title: string;
+    description: string;
+  };
+};
+
+export type FormField = FieldTypeRadio | FieldTypeQuestion;
+
+export interface Form {
+  title: string;
+  description?: string;
+  fields: [FormField];
 }

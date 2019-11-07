2700381461b553e5731420e2f9a0faf4a38fd169
Idan Lottan <idan2110@gmail.com>
2019-11-07 15:59:33 +0200

    create basic schema for the Form model

--- packages/server/src/models/Form.ts
+++ packages/server/src/models/Form.ts
@@ -1,35 +1,61 @@
 import mongoose from 'mongoose';
+import { Form as FormDocument } from '@formable/shared';
 
-export type FormDocument = mongoose.Document & {
-  title: string;
-};
+const fieldTypeQuestionSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String
+  },
+  { _id: false }
+);
 
-export type FieldTypeQuestion = {
-  type: 'question';
-  question: {
-    title: string;
-    description: string;
-  };
-};
+const radioOptionSchema = new mongoose.Schema(
+  {
+    label: { required: true, type: String },
+    value: { required: true, type: String }
+  },
+  { _id: false }
+);
 
-export type FieldTypeRadio = {
-  type: 'radio';
-  radio: {
-    title: string;
-    options: Array<{ label: string; value: string }>;
-  };
-};
+const fieldTypeRadioSchema = new mongoose.Schema(
+  {
+    title: String,
+    options: [radioOptionSchema]
+  },
+  { _id: false }
+);
 
-export type Field = mongoose.Document & (FieldTypeQuestion | FieldTypeRadio);
-
-const fieldSchema = new mongoose.Schema({});
+const fieldSchema = new mongoose.Schema(
+  { required: { required: false, type: Boolean, default: false } },
+  { discriminatorKey: 'type', _id: false }
+);
 
 const formSchema = new mongoose.Schema(
   {
-    title: String,
+    title: { required: true, type: String },
+    description: String,
     fields: [fieldSchema]
   },
   { timestamps: true }
 );
 
-export const Form = mongoose.model<FormDocument>('Form', formSchema);
+(formSchema.path('fields') as any).discriminator(
+  'question',
+  new mongoose.Schema(
+    { question: { required: true, type: fieldTypeQuestionSchema } },
+    { _id: false }
+  )
+);
+
+(formSchema.path('fields') as any).discriminator(
+  'radio',
+  new mongoose.Schema(
+    { radio: { required: true, type: fieldTypeRadioSchema } },
+    { _id: false }
+  )
+);
+
+export const Form = mongoose.model<FormDocument & mongoose.Document>(
+  'Form',
+  formSchema
+);

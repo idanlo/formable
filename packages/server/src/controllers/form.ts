6433b5e27d2b6fe876d054a905ae27fa00c0b529
Idan Lottan <idan2110@gmail.com>
2019-12-26 20:14:11 +0200

    remove user._id typescript error by referencing UserDocument type

--- packages/server/src/controllers/form.ts
+++ packages/server/src/controllers/form.ts
@@ -2,6 +2,7 @@
 
 import express, { Response, Request } from 'express';
 import { Form as FormType } from '@formable/shared';
+import { UserDocument } from '../models/User';
 import { Form } from '../models/Form';
 import { isAuthenticated } from '../config/passport';
 
@@ -10,7 +11,7 @@ const router = express.Router();
 router.post('/create', isAuthenticated, async (req: Request, res: Response) => {
   try {
     const form = req.body as FormType;
-    form.owner = req.user._id;
+    form.owner = (req.user as UserDocument)._id;
     const newForm = new Form(form);
     await newForm.save();
     return res.status(200).json(newForm);

273106d6615bec8f81040b029eafaf856fe2ee84
Idan Lottan <idan2110@gmail.com>
2020-01-03 18:12:10 +0200

    @client - App.tsx, check if client is logged in, show loading text while loading, and show FormBuilder if logged in

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import Header from './containers/Header';
-import FormBuilder from './containers/FormBuilder';
+import FormBuilder from './containers/FormBuilder/FormBuilder';
 import './App.css';
 
 const App: React.FC = () => {
-  // React.useEffect(() => {
-  //   axios({
-  //     url: '/api/auth/test',
-  //     method: 'GET'
-  //     // url: '/api/auth/signup',
-  //     // method: 'POST',
-  //     // data: {
-  //     //   email: 'idan21120@gmail.com',
-  //     //   password: '1234567'
-  //     // }
-  //   })
-  //     .then(res => {
-  //       console.log(res);
-  //     })
-  //     .catch(err => {
-  //       console.log(err.response.data.message);
-  //     });
-  // }, []);
+  const [loading, setLoading] = React.useState(true);
+  const [authenticated, setAuthenticated] = React.useState(true);
+
+  React.useEffect(() => {
+    axios({
+      // url: '/api/auth/test',
+      // method: 'GET'
+      // url: '/api/auth/signup',
+      // method: 'POST',
+      // data: {
+      //   email: 'idan21120@gmail.com',
+      //   password: '1234567'
+      // }
+      url: '/api/auth/is-logged',
+      method: 'GET'
+    })
+      .then(res => {
+        setAuthenticated(true);
+        setLoading(false);
+        console.log(res);
+      })
+      .catch(err => {
+        setAuthenticated(false);
+        setLoading(false);
+        console.log(err.response.data.message);
+      });
+  }, []);
 
   return (
     <div>
-      <Header />
-      <FormBuilder />
+      {loading ? (
+        <p>Loading...</p>
+      ) : authenticated ? (
+        <>
+          <Header />
+          <FormBuilder />
+        </>
+      ) : (
+        <p>Not authenticated</p>
+      )}
     </div>
   );
 };
